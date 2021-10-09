f703a8f30473dffd10e565148b093650eda30123
Raghav Agrawal <agrawal0019@hotmail.com>
2021-10-09 13:26:34 +0530

    Added Delegate and Events demo project

---
+++ DelegateAndEvents/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoEncoder encoder = new VideoEncoder();      //Publisher
+
+            MailService mail = new MailService();       //Subscriber
+            MessageService msg = new MessageService();       //Subscriber
+            encoder.VideoEncoded += mail.SendEmail;
+            encoder.VideoEncoded += msg.SendMessage;
+
+            encoder.Encode();
+            Console.Read();
+            
+        }
+
+    }
+
+    class MailService {
+
+        public void SendEmail() {
+            
+            Console.WriteLine("Email Sent!");
+        }
+    
+    }
+
+    class MessageService {
+        public void SendMessage()
+        {
+            Console.WriteLine("Message Sent!");
+        }
+
+    }
+
+}
---
+++ DelegateAndEvents/VideoEncoder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvents
+{
+    public class VideoEncoder
+    {
+        //1. Define a delegate
+        public delegate void VideoEncodedEventHandler();
+        //2.Define a event based on that delegate
+        public VideoEncodedEventHandler VideoEncoded;
+
+        public void Encode()
+        {
+            Thread.Sleep(3000);     //Do Code
+            Console.WriteLine("Video Encoded");
+            if (VideoEncoded != null)
+            {
+                VideoEncoded();    //Raise Event
+            }
+
+        }
+    }
+}
