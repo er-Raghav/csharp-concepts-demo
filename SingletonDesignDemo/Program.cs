2490c9b133bb9fa549a6209cad40f0a7a072868c
Raghav Agrawal <agrawal0019@hotmail.com>
2021-10-23 21:53:49 +0530

    Singleton Design Pattern

---
+++ SingletonDesignDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            logger objlog = logger.getInstance;
+
+            objlog.writetrace();
+
+            logger objlog1 = logger.getInstance;
+
+            objlog1.writetrace();
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ SingletonDesignDemo/logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignDemo
+{
+    sealed class logger                        //Class should be sealed
+    {
+        private static int counter;
+        private static logger _logInstance;
+        private logger()                        //All constructors must be private
+        {
+            counter++;
+            
+        }
+        public static logger getInstance
+        {
+            get
+            {
+                if (_logInstance == null)
+                {
+                    _logInstance = new logger();            //Object Creation inside static property.
+                }
+                return _logInstance;
+            }
+        }
+
+        public void writetrace() {
+            Console.WriteLine($"Object counter :{counter}");
+            Console.WriteLine("This method is writing trace");
+        }
+
+    }
+}
